c1901a4cac160e420917bd473cc5efe1d5f064b2
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-03-23 19:31:37 +0200

    added console adapter

---
+++ lua/mvp/adapters/logger/console.lua
@@ -0,0 +1,26 @@
+local adapter = mvp.meta.logger:New()
+
+function adapter:Log( level, ... )
+    local logTime = os.date( '%Y-%m-%d %H:%M:%S' )
+    local levelText, levelColor = 'INFO', mvp.BLUE
+
+    if level == mvp.LOG_ERROR then
+        levelText, levelColor = 'ERROR', mvp.RED
+    elseif level == mvp.LOG_WARNING then
+        levelText, levelColor = 'WARNING', mvp.YELLOW
+    end
+
+    if level == mvp.LOG_DEBUG then
+        levelText, levelColor = 'DEBUG', mvp.GREEN
+        if mvp.DEBUG then
+            MsgC( mvp.BLUE, '[MVP | Terminal]', levelColor, '[', levelText, '] ', mvp.GRAY, '[', mvp.WHITE, logTime, mvp.GRAY, '] ', ... )
+        end
+    else
+        MsgC( mvp.BLUE, '[MVP | Terminal]', levelColor, '[', levelText, '] ', mvp.GRAY, '[', mvp.WHITE, logTime, mvp.GRAY, '] ', ... )
+    end
+
+    
+    MsgC('\n')
+end
+
+return adapter
