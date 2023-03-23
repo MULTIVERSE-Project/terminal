e992bfc59506486837ddb0facc900824be835667
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-03-23 19:31:28 +0200

    added file adapter

---
+++ lua/mvp/adapters/logger/file.lua
@@ -0,0 +1,72 @@
+local adapter = mvp.meta.logger:New()
+
+adapter.logfilePath = 'mvp/logs/'
+adapter.currentWorkingFile = nil
+
+function adapter:Init()
+    local currentTimestamp = os.date('%Y-%m-%d')
+    local logFileName = 'log-' .. currentTimestamp
+
+    if not file.Exists(self.logfilePath, 'DATA') then
+        file.CreateDir(self.logfilePath)
+    end
+
+    if not file.Exists(self.logfilePath .. logFileName .. '.txt', 'DATA') then
+        file.Write(self.logfilePath .. logFileName .. '.txt', 'This is a start of log file for ' .. currentTimestamp .. '\n')
+    end
+    self.currentWorkingFile = self.logfilePath .. logFileName .. '.txt'
+end
+
+function adapter:WriteToFile(message)
+    local logTime = os.date('%Y-%m-%d %H:%M:%S')
+
+    if self.currentWorkingFile then
+        file.Append(self.currentWorkingFile, '[' .. logTime .. '] ' .. message .. '\n')
+    end
+end
+
+function adapter:SanitazeMessage(message)
+    local sanitizedMessage = ''
+
+    if type(message) == 'table' then
+        for k, v in pairs(message) do
+            if type(v) == 'table' then
+                continue -- skip tables
+            else
+                sanitizedMessage = sanitizedMessage .. tostring(v)
+            end
+        end
+    else
+        sanitizedMessage = tostring(message)
+    end
+
+    return sanitizedMessage
+end
+
+function adapter:Log(level, ...)
+    if not self.currentWorkingFile then
+        self:Init()
+    end
+
+    local message = self:SanitazeMessage({...})
+
+    local levelText = 'INFO'
+    if level == mvp.LOG_ERROR then
+        levelText = 'ERROR'
+    elseif level == mvp.LOG_WARNING then
+        levelText = 'WARNING'
+    end
+
+    if level == mvp.LOG_DEBUG then
+        levelText = 'DEBUG'
+        if mvp.DEBUG then
+            self:WriteToFile('[' .. levelText .. '] ' .. message)
+        end
+    else
+        self:WriteToFile('[' .. levelText .. '] ' .. message)
+    end
+
+    print('[' .. levelText .. '] ' .. message)
+end
+
+return adapter
