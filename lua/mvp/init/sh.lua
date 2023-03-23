844e4662137088f053ca4a1dbbe4d34dce69f4e3
Kot <44447172+Kotyarishka@users.noreply.github.com>
2023-03-23 19:31:00 +0200

    added logger itselft

--- lua/mvp/init/sh.lua
+++ lua/mvp/init/sh.lua
@@ -9,10 +9,20 @@ mvp.BLUE = Color(0, 102, 255)
 mvp.CYAN = Color(0, 168, 255)
 mvp.YELLOW = Color(255, 255, 0, 255)
 mvp.ORANGE = Color(225, 177, 44)
+mvp.GRAY = Color(207, 207, 207)
+ 
+-- log levels
+mvp.LOG_DEBUG = 0
+mvp.LOG_INFO = 1
+mvp.LOG_WARNING = 2
+mvp.LOG_ERROR = 3  
 
 -- Load loader for later use
 if SERVER then AddCSLuaFile('mvp/core/sh_loader.lua') end
 include('mvp/core/sh_loader.lua')
 
-mvp.loader.LoadSvFile('init/sv.lua')
-mvp.loader.LoadClFile('init/cl.lua')
+mvp.loader.LoadServerFile('init/sv.lua')
+mvp.loader.LoadClientFile('init/cl.lua')
+
+mvp.loader.LoadFolder('core/logger')
+mvp.logger.Init()
