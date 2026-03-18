aa9d5e8ace5d11baa6d63a7fdeb9747a2aa87c97
Kotyarishka <44447172+Kotyarishka@users.noreply.github.com>
2026-03-18 16:57:38 +0200

    add: temp benchmarks

--- lua/mvp/loggers/console.lua
+++ lua/mvp/loggers/console.lua
@@ -31,5 +31,57 @@ function LOGGER:Log(level, caller, ...)
         "\n"
     )
 end
+function LOGGER:LogOld(level, caller, ...)
+    local shouldDisplayDebug = mvp.config and mvp.config.Get("debug", false)
+    if (not shouldDisplayDebug and level == mvp.LOG.DEBUG) then
+        return
+    end
+
+    local color = LEVEL_TO_COLOR_MAP[level] or Color(255, 255, 255)
+
+    local msg = {}
+
+    msg[#msg + 1] = color
+    msg[#msg + 1] = "▌ "
+    msg[#msg + 1] = mvp.LOG[level]
+    msg[#msg + 1] = "\t"
+    msg[#msg + 1] = Color(255, 255, 255)
+    
+    if (caller) then
+        msg[#msg + 1] = "["
+        msg[#msg + 1] = Color(255, 255, 0)
+        msg[#msg + 1] = caller
+        msg[#msg + 1] = Color(255, 255, 255)
+        msg[#msg + 1] = "] "
+    end
+
+    table.Add(msg, {...})
+
+    MsgC(unpack(msg))
+    Msg("\n")
+end
 
 mvp.logger.Register(LOGGER)
+
+local function testNew()
+    LOGGER:Log(mvp.LOG.INFO, "TestCaller", "This is an info message")
+end
+local function testNewNoCaller()
+    LOGGER:Log(mvp.LOG.INFO, nil, "This is an info message")
+end
+local function testOld()
+    LOGGER:LogOld(mvp.LOG.INFO, "TestCaller", "This is an info message")
+end
+local function testOldNoCaller()
+    LOGGER:LogOld(mvp.LOG.INFO, nil, "This is an info message")
+end
+
+concommand.Add("mvp_log_test", function()
+    LuctusCompareOften(10, 0.1, 1000, {
+        {"new implementation, with caller", testNew},
+        {"new implementation, without caller", testNewNoCaller},
+
+        {"old implementation, with caller", testOld},
+        {"old implementation, without caller", testOldNoCaller}
+    })
+end)
